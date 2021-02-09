009e47b7aa22ae078ee863eccaf6b76543f7da24
Ziad K <zkhalafeu01@tutanota.com>
2021-02-09 18:30:34 +0100

    Added models Device CRUD operations

--- models/device.go
+++ models/device.go
@@ -34,10 +34,10 @@ func (device *Device) Validate() (map[string]interface{}, bool) {
 		return u.Message(false, "Domain should be on the payload"), false
 	}
 
-	if GetDB().Table("racks").
-		Where("id = ?", device.Domain).First(&Rack{}).Error != nil {
+	if GetDB().Table("devices").
+		Where("id = ?", device.Domain).First(&Device{}).Error != nil {
 
-		return u.Message(false, "Domain should be correspond to Rack ID"), false
+		return u.Message(false, "Domain should be correspond to Device ID"), false
 	}
 
 	if device.Color == "" {
@@ -69,10 +69,10 @@ func (device *Device) Create() map[string]interface{} {
 	return resp
 }
 
-//Get the first device given the rack
-func GetDevice(rack *Rack) *Device {
+//Get the device given the ID
+func GetDevice(id uint) *Device {
 	device := &Device{}
-	err := GetDB().Table("devices").Where("foreignkey = ?", rack.ID).First(device).Error
+	err := GetDB().Table("devices").Where("id = ?", id).First(device).Error
 	if err != nil {
 		fmt.Println(err)
 		return nil
@@ -98,3 +98,68 @@ func GetDevices(rack *Rack) []*Device {
 //Need Update and Delete
 //These would be a bit more complicated
 //So leave them out for now
+
+func UpdateDevice(id uint, newDeviceInfo *Device) map[string]interface{} {
+	device := &Device{}
+
+	err := GetDB().Table("devices").Where("id = ?", id).First(device).Error
+	if err != nil {
+		return u.Message(false, "Device was not found")
+	}
+
+	if newDeviceInfo.Name != "" && newDeviceInfo.Name != device.Name {
+		device.Name = newDeviceInfo.Name
+	}
+
+	if newDeviceInfo.Category != "" && newDeviceInfo.Category != device.Category {
+		device.Category = newDeviceInfo.Category
+	}
+
+	if newDeviceInfo.Desc != "" && newDeviceInfo.Desc != device.Desc {
+		device.Desc = newDeviceInfo.Desc
+	}
+
+	//Should it be possible to update domain
+	// Will have to think about it more
+	//if newDeviceInfo.Domain
+
+	if newDeviceInfo.Color != "" && newDeviceInfo.Color != device.Color {
+		device.Color = newDeviceInfo.Color
+	}
+
+	if newDeviceInfo.Orientation != "" {
+		switch newDeviceInfo.Orientation {
+		case "NE", "NW", "SE", "SW":
+			device.Orientation = newDeviceInfo.Orientation
+
+		default:
+		}
+	}
+
+	//Successfully validated the new data
+	GetDB().Table("devices").Save(device)
+	return u.Message(true, "success")
+}
+
+func DeleteDevice(id uint) map[string]interface{} {
+
+	//First check if the device exists
+	err := GetDB().Table("devices").Where("id = ?", id).First(&Device{}).Error
+	if err != nil {
+		fmt.Println("Couldn't find the device to delete")
+		return nil
+	}
+
+	//This is a hard delete!
+	e := GetDB().Unscoped().Table("devices").Delete(&Device{}, id).Error
+
+	//The command below is a soft delete
+	//Meaning that the 'deleted_at' field will be set
+	//the record will remain but unsearchable
+	//e := GetDB().Table("tenants").Delete(Tenant{}, id).Error
+	if e != nil {
+		return u.Message(false, "There was an error in deleting the device")
+	}
+
+	return u.Message(true, "success")
+}
