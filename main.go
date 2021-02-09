3a7aeb2d5210971350f4b1474a35b979d1d7b7c0
Ziad K <zkhalafeu01@tutanota.com>
2021-02-09 11:29:27 +0100

    Cleaned out test case comments in main

--- main.go
+++ main.go
@@ -1,47 +1,5 @@
 package main
 
-//Test Case 1
-//curl -X POST http://localhost:8000/api/user/login
-//--data '{"email": "iamlegend@gmail.com", "password" : "secret"}'
-
-//Test Case 2
-//curl -H 'Accept: application/json'
-//-H "Authorization: Bearer ${jj}" http://localhost:8000/api/me/contacts
-
-//Test Case 3
-//curl -X GET -H 'Content-Type: application/json' \
-//-H 'Authorization: bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
-//eyJVc2VySWQiOjYyNTgwMjc5ODYzNjIwNDAzM30.
-//FM-K77j5439O1irfJU_O8Rx7VlVkyGpuwmi87tWLTzU' \
-//-i 'http://127.0.0.1:8000/api/me/contacts'
-
-//Test Case 4 == FALSE
-//curl -X POST http://localhost:8000/api/user/login
-//--data '{"email": "realcheat@gmail.com", "password": "secret"}'
-
-//Test Case 5
-//curl -X POST -H 'Content-Type: application/json'
-//-H 'Authorization: bearer TOKEN'
-//--data '{"name": "SiteC", "category": "stupid",
-//"domain": 630085240294703105, "description": "Cant evalutate anything",
-//"color": "orange", "eorientation": "NW"}'
-//-i 'http://localhost:8000/api/user/sites/new'
-
-//Test Case 6
-//curl -X POST -H 'Content-Type: application/json'
-//-H 'Authorization: bearer TOKEN'
-//--data '{"name": "CEA", "category": "research",
-//"description": "Cant evalutate anything", "color": "red"}'
-//-i 'http://localhost:8000/api/user/tenants/new'
-
-//Test Case 7
-//curl -X POST http://localhost:8000/api/user/new
-//--data '{"email": "iamlegend@gmail.com", "password" : "secret"}'
-
-//Test Case 9
-//curl -X GET
-//http://localhost:8000/api/user/sites -H 'Authorization: bearer TOKEN'
-
 import (
 	"fmt"
 	"p3/app"
