0e32703a384f0553b57b23d3e1fe4f0d65e1ddf6
Ziad K <zkhalafeu01@tutanota.com>
2021-02-12 20:39:03 +0100

    Added doc annotations for Building

--- controllers/buildingControllers.go
+++ controllers/buildingControllers.go
@@ -10,6 +10,55 @@ import (
 	"github.com/gorilla/mux"
 )
 
+// swagger:operation POST /api/user/buildings buildings Create
+// Creates a Building in the system
+// ---
+// produces:
+// - application/json
+// parameters:
+// - name: Name
+//   in: query
+//   description: Name of building
+//   required: true
+//   type: string
+//   default: "Building A"
+// - name: Category
+//   in: query
+//   description: Category of Building (ex. Consumer Electronics, Medical)
+//   required: true
+//   type: string
+//   default: "Research"
+// - name: Description
+//   in: query
+//   description: Description of Building
+//   required: true
+//   type: string
+//   default: "Some abandoned building in Grenoble"
+// - name: Domain
+//   description: 'This an attribute that refers to
+//   an existing parent'
+//   required: true
+//   type: int
+//   default: 999
+// - name: Color
+//   in: query
+//   description: Color of Building (useful for 3D rendering)
+//   required: true
+//   type: string
+//   default: "Silver"
+// - name: Orientation
+//   in: query
+//   description: 'Indicates the location. Only values of
+//   "NE", "NW", "SE", "SW" are acceptable'
+//   required: true
+//   type: string
+//   default: "NE"
+// responses:
+//     '200':
+//         description: Created
+//     '400':
+//         description: Bad request
+
 var CreateBuilding = func(w http.ResponseWriter, r *http.Request) {
 
 	bldg := &models.Building{}
@@ -23,6 +72,23 @@ var CreateBuilding = func(w http.ResponseWriter, r *http.Request) {
 	u.Respond(w, resp)
 }
 
+// swagger:operation GET /api/user/buildings/{id} buildings GetBuilding
+// Gets Building using Building ID.
+// ---
+// produces:
+// - application/json
+// parameters:
+// - name: ID
+//   in: path
+//   description: ID of Building
+//   required: true
+//   type: int
+//   default: 999
+// responses:
+//     '200':
+//         description: Found
+//     '400':
+//         description: Not Found
 //Retrieve bldg using Bldg ID
 var GetBuilding = func(w http.ResponseWriter, r *http.Request) {
 
@@ -42,6 +108,23 @@ var GetBuilding = func(w http.ResponseWriter, r *http.Request) {
 	u.Respond(w, resp)
 }
 
+// swagger:operation DELETE /api/user/buildings/{id} buildings DeleteBuilding
+// Deletes a Building.
+// ---
+// produces:
+// - application/json
+// parameters:
+// - name: ID
+//   in: path
+//   description: ID of desired building
+//   required: true
+//   type: int
+//   default: 999
+// responses:
+//     '204':
+//        description: Successful
+//     '400':
+//        description: Not found
 var DeleteBuilding = func(w http.ResponseWriter, r *http.Request) {
 	id, e := strconv.Atoi(mux.Vars(r)["id"])
 
@@ -53,6 +136,57 @@ var DeleteBuilding = func(w http.ResponseWriter, r *http.Request) {
 	u.Respond(w, v)
 }
 
+// swagger:operation PUT /api/user/buildings/{id} buildings UpdateBuilding
+// Changes Building data in the system.
+// If no new or any information is provided
+// an OK will still be returned
+// ---
+// produces:
+// - application/json
+// parameters:
+// - name: ID
+//   in: path
+//   description: ID of desired building
+//   required: true
+//   type: int
+//   default: 999
+// - name: Name
+//   in: query
+//   description: Name of building
+//   required: false
+//   type: string
+//   default: "Building B"
+// - name: Category
+//   in: query
+//   description: Category of Building (ex. Consumer Electronics, Medical)
+//   required: false
+//   type: string
+//   default: "Research"
+// - name: Description
+//   in: query
+//   description: Description of Building
+//   required: false
+//   type: string
+//   default: "Some abandoned building in Grenoble"
+// - name: Color
+//   in: query
+//   description: Color of Building (useful for 3D rendering)
+//   required: false
+//   type: string
+//   default: "Blue"
+// - name: Orientation
+//   in: query
+//   description: 'Indicates the location. Only values of
+//   "NE", "NW", "SE", "SW" are acceptable'
+//   required: false
+//   type: string
+//   default: "NE"
+
+// responses:
+//     '200':
+//         description: Updated
+//     '400':
+//         description: Bad request
 //Updates work by passing ID in path parameter
 var UpdateBuilding = func(w http.ResponseWriter, r *http.Request) {
 
