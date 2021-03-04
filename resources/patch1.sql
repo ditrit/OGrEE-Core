fd62dd6f41d9105e0f65030277f491bab250540a
Ziad K <zkhalafeu01@tutanota.com>
2021-03-04 23:33:56 +0100

    Created patch file to make DB better match the Unity JSON files

---
+++ resources/patch1.sql
@@ -0,0 +1,11 @@
+
+ALTER TABLE rack_attributes ADD rack_vendor text;
+ALTER TABLE rack_attributes ADD rack_type text;
+ALTER TABLE rack_attributes ADD rack_model text;
+ALTER TABLE rack_attributes ADD rack_serial text;
+
+
+ALTER TABLE device_attributes ADD device_vendor text;
+ALTER TABLE device_attributes ADD device_type text;
+ALTER TABLE device_attributes ADD device_model text;
+ALTER TABLE device_attributes ADD device_serial text;
