9e3d072ccd000534b2a79de79668544a3369c0c9
Ziad K <zkhalafeu01@tutanota.com>
2021-03-05 21:14:05 +0100

    Fixed mistake in patch1.sql

--- resources/patch1.sql
+++ resources/patch1.sql
@@ -16,11 +16,11 @@ ALTER TABLE device_attributes ADD device_serial text;
 
 SET enable_experimental_alter_column_type_general = true;
 alter table device alter column device_description SET DATA TYPE text;
-alter table rack alter column device_description SET DATA TYPE text;
-alter table room alter column device_description SET DATA TYPE text;
-alter table building alter column device_description SET DATA TYPE text;
-alter table site alter column device_description SET DATA TYPE text;
-alter table tenant alter column device_description SET DATA TYPE text;
+alter table rack alter column rack_description SET DATA TYPE text;
+alter table room alter column room_description SET DATA TYPE text;
+alter table building alter column building_description SET DATA TYPE text;
+alter table site alter column site_description SET DATA TYPE text;
+alter table tenant alter column tenant_description SET DATA TYPE text;
 SET enable_experimental_alter_column_type_general = false;
 
 
