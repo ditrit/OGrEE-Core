b8eb7a34136a144359661d7c1404894ce9f5a0f8
Ziad K <zkhalafeu01@tutanota.com>
2021-03-05 18:32:17 +0100

    Added comments to patch1.sql describing how to modify the DB description columns to make

--- resources/patch1.sql
+++ resources/patch1.sql
@@ -9,3 +9,19 @@ ALTER TABLE device_attributes ADD device_vendor text;
 ALTER TABLE device_attributes ADD device_type text;
 ALTER TABLE device_attributes ADD device_model text;
 ALTER TABLE device_attributes ADD device_serial text;
+
+
+-- Possible option to change description columns
+-- to single string
+
+--SET enable_experimental_alter_column_type_general = true;
+-- alter table device alter column device_description SET DATA TYPE text;
+-- alter table rack alter column device_description SET DATA TYPE text;
+-- alter table room alter column device_description SET DATA TYPE text;
+-- alter table building alter column device_description SET DATA TYPE text;
+-- alter table site alter column device_description SET DATA TYPE text;
+-- alter table tenant alter column device_description SET DATA TYPE text;
+--SET enable_experimental_alter_column_type_general = false;
+
+
+
