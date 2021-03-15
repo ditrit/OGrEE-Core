32dab15ff8b9dc61a324bfe17d09c41753134676
Ziad K <zkhalafeu01@tutanota.com>
2021-03-15 23:07:21 +0100

    Fixed mistake in error response message in ValidateRack

--- models/rack.go
+++ models/rack.go
@@ -61,7 +61,7 @@ func (rack *Rack) Validate() (map[string]interface{}, bool) {
 	if GetDB().Table("room").
 		Where("id = ?", rack.ParentID).First(&Room{}).Error != nil {
 
-		return u.Message(false, "ParentID should be correspond to building ID"), false
+		return u.Message(false, "ParentID should be correspond to Room ID"), false
 	}
 
 	if rack.Attributes.PosXY == "" {
