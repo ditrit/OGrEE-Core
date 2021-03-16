472bad17fac11ba93235bdea8b4564886a001941
Ziad K <zkhalafeu01@tutanota.com>
2021-03-16 20:02:19 +0100

    Added a map int string variable which would be useful for a more sophisticated error system

--- app/errors.go
+++ app/errors.go
@@ -5,6 +5,19 @@ import (
 	u "p3/utils"
 )
 
+//A more sophisticated error system might be
+//better later on
+/*
+var ErrorCatalogue map[int]string{
+	0: "",
+	1: "",
+	2: "",
+	3: "",
+	4: "",
+	5: "",
+}
+*/
+
 var NotFoundHandler = func(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
