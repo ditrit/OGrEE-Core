d1aa7c93af094faf6d603459bd4f1a33edc7152c
Ziad K <zkhalafeu01@tutanota.com>
2021-05-20 12:38:15 +0200

    Removed useless endpoint from Room and updated doc

--- main.go
+++ main.go
@@ -259,9 +259,6 @@ func main() {
 	router.HandleFunc("/api/user/rooms/{id:[0-9]+}/racks",
 		controllers.GetRacksOfParent).Methods("GET")
 
-	router.HandleFunc("/api/user/rooms/{id:[0-9]+}/devices",
-		controllers.GetDevicesOfRoom).Methods("GET")
-
 	router.HandleFunc("/api/user/rooms/{id:[0-9]+}/all",
 		controllers.GetRoomHierarchy).Methods("GET")
 
