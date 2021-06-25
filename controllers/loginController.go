2852d6cba0ec391835110a116ce01d77c675105d
Ziad K <zkhalafeu01@tutanota.com>
2021-06-25 13:26:40 +0200

    Everything works so far but a bug exists for new users. Generating array nodes func seems to think that the json response is different when a new user signs up and causes a crash when populating the Tree. However, the new user can continue as usual if the prog is restarted

---
+++ controllers/loginController.go
@@ -0,0 +1,89 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strconv"
+	"strings"
+
+	"github.com/chzyer/readline"
+	"github.com/joho/godotenv"
+)
+
+func CreateCredentials() (string, string) {
+	var tp map[string]interface{}
+	var key string
+	client := &http.Client{}
+
+	user, _ := readline.Line("Please Enter desired user email: ")
+	pass, _ := readline.Password("Please Enter desired password: ")
+
+	buf, _ := json.Marshal(map[string]interface{}{"email": user,
+		"password": pass})
+
+	req, _ := http.NewRequest("POST",
+		"https://ogree.chibois.net/api/user",
+		bytes.NewBuffer(buf))
+
+	resp, e := client.Do(req)
+	if e != nil || resp.StatusCode != http.StatusCreated {
+		println("Error while creating credentials on server! Now exiting")
+		os.Exit(-1)
+	}
+	defer resp.Body.Close()
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		readline.Line("Error: " + err.Error() + " Now Exiting")
+		os.Exit(-1)
+	}
+	json.Unmarshal(bodyBytes, &tp)
+	key = (tp["account"].(map[string]interface{}))["token"].(string)
+
+	os.WriteFile("./.resources/.env",
+		[]byte("user="+user+"\n"+"apikey="+key),
+		0666)
+
+	return user, key
+}
+
+func CheckKeyIsValid(key string) bool {
+	client := &http.Client{}
+
+	req, _ := http.NewRequest("GET",
+		"https://ogree.chibois.net/api/token/valid", nil)
+
+	req.Header.Set("Authorization", "Bearer "+key)
+
+	resp, e := client.Do(req)
+	if e != nil || resp.StatusCode != 200 {
+		//readline.Line(e.Error())
+		readline.Line("Status code" + strconv.Itoa(resp.StatusCode))
+		return false
+	}
+	return true
+}
+
+func Login() (string, string) {
+	println("LOGGING IN NOW")
+	var user, key string
+	e := godotenv.Load(".resources/.env")
+	if e != nil {
+		user, key = CreateCredentials()
+	} else {
+		user = os.Getenv("user")
+		key = os.Getenv("apikey")
+		if !CheckKeyIsValid(key) {
+			println("Error while checking key. Now exiting")
+			os.Exit(-1)
+		}
+	}
+
+	println("Checking credentials...")
+	println(CheckKeyIsValid(key))
+
+	user = (strings.Split(user, "@"))[0]
+	return user, key
+}
