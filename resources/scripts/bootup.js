860ef6b058f231fc6bad9aca86cad9985004f7b8
denizul <ziad.khalaf@orness.com>
2022-06-17 17:10:38 -0400

    Added additional DB user strictly for making backups

--- resources/scripts/bootup.js
+++ resources/scripts/bootup.js
@@ -8,11 +8,16 @@
 var m = new Mongo()
 var authDB = m.getDB("admin")
 
+//Create the Admin user
 db.createUser({ user: "admin", pwd: "adminpassword",
                 roles: [{role: "userAdminAnyDatabase", db: "admin"},
                 { role: "readWriteAnyDatabase", db: "admin"}]
                 })
 
+//Create the Backup user named guard
+db.createUser({ user: "guard", pwd: "adminpassword",
+                roles: [{role: "backup", db: "admin"}, {role: "restore", db: "admin"}]
+                })
 
 var db = m.getDB("ogree");
 db.createCollection('customer');
