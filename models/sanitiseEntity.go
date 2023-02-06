280f3d36ea9e37cf763fef4458d6b34e6d02ab61
helderbetiol <helderbetiol@gmail.com>
2023-02-06 07:29:11 -0300

    Update sanitiseEntity.go

--- models/sanitiseEntity.go
+++ models/sanitiseEntity.go
@@ -1,13 +1,14 @@
 package models
 
 import (
+	u "p3/utils"
 	"strings"
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-//Mongo returns '_id' instead of id
+// Mongo returns '_id' instead of id
 func fixID(data map[string]interface{}) map[string]interface{} {
 	if v, ok := data["_id"]; ok {
 		data["id"] = v
@@ -16,7 +17,7 @@ func fixID(data map[string]interface{}) map[string]interface{} {
 	return data
 }
 
-//Removes underscore in object category if present
+// Removes underscore in object category if present
 func FixUnderScore(x map[string]interface{}) {
 	if catInf, ok := x["category"]; ok {
 		if cat, _ := catInf.(string); strings.Contains(cat, "_") == true {
@@ -25,9 +26,9 @@ func FixUnderScore(x map[string]interface{}) {
 	}
 }
 
-//Perform any neccessary adjustments to objects before insertion into DB
+// Perform any neccessary adjustments to objects before insertion into DB
 func FixAttributesBeforeInsert(entity int, data map[string]interface{}) {
-	if entity == RACK {
+	if entity == u.RACK {
 		pid, _ := primitive.ObjectIDFromHex(data["parentId"].(string))
 		req := bson.M{"_id": pid}
 		parent, _ := GetEntity(req, "room")
