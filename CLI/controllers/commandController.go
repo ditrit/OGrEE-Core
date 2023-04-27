378ed5ba3396f6f87653b97cebc9fd1f31e3ac48
Gérémy Hutin <gerem.hutin@gmail.com>
2023-04-27 14:03:30 +0200

    type check for temperatures is done by the schema rather than the CLI (#34)
    * allows attributes to have dash or underscore
    
    * removes cli check, adds it to device schema

--- CLI/controllers/commandController.go
+++ CLI/controllers/commandController.go
@@ -513,23 +513,7 @@ func UpdateObj(Path, id, ent string, data map[string]interface{}, deleteAndPut b
 					} else { //Description is some invalid value
 						objJSON["description"] = []interface{}{data[i]}
 					}
-
 				}
-
-				if strings.Contains(i, "temperature_") {
-					category := EntityStrToInt(objJSON["category"].(string))
-					if category == RACK || category == DEVICE ||
-						category == ROOM {
-						switch data[i].(type) {
-						case float64, float32: //GOOD
-						default:
-							msg := "The temperature for sensors should be a float"
-							return nil, fmt.Errorf(msg)
-						}
-					}
-
-				}
-
 				if i == "usableColor" || i == "reservedColor" ||
 					i == "technicalColor" {
 					category := EntityStrToInt(objJSON["category"].(string))
