a58130bb1df9a71e0dfb1ba128e6edb52f23a47e
helderbetiol <helderbetiol@gmail.com>
2023-05-17 11:56:41 +0200

    Add logo and .env for runtime

--- APP/ogree_app/lib/main.dart
+++ APP/ogree_app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:ogree_app/pages/login_page.dart';
 import 'package:ogree_app/pages/reset_page.dart';
 
 Future<void> main() async {
+  await dotenv.load(fileName: "assets/custom/.env");
   runApp(const MyApp());
 }
 
