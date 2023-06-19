d783539e9be30310978bfbed9cca25577d2c6762
Gérémy Hutin <gerem.hutin@gmail.com>
2023-06-19 12:23:34 +0200

    removes remaining "status" field check

--- CLI/utils/util.go
+++ CLI/utils/util.go
@@ -21,10 +21,6 @@ func ExeDir() string {
 	return filepath.Dir(exe)
 }
 
-func Message(status bool, message string) map[string]interface{} {
-	return map[string]interface{}{"status": status, "message": message}
-}
-
 func Respond(w http.ResponseWriter, data map[string]interface{}) {
 	w.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
