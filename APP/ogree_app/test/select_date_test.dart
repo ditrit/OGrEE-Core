f86d08d020512cf6888f4636be74607da554b35b
helderbetiol <helderbetiol@gmail.com>
2023-06-29 12:01:30 +0200

    Fix tests

--- APP/ogree_app/test/select_date_test.dart
+++ APP/ogree_app/test/select_date_test.dart
@@ -7,15 +7,16 @@ void main() {
   testWidgets('SelectDate can toogle between tabs', (tester) async {
     await tester.pumpWidget(LocalizationsInjApp(child: SelectDate()));
     expect(find.text('Choisir les dates'), findsOneWidget);
+    expect(find.textContaining('disponibles'), findsWidgets);
 
-    await tester.tap(find.textContaining("dernier"));
+    await tester.tap(find.textContaining("Choisir les dates"));
     await tester.pumpAndSettle();
-    expect(find.textContaining('Données mises à jour le'), findsOneWidget);
+    expect(find.textContaining('disponibles'), findsNothing);
 
-    await tester.ensureVisible(find.textContaining("enregistré"));
+    await tester.ensureVisible(find.textContaining("Toutes"));
     await tester.pumpAndSettle();
-    await tester.tap(find.textContaining("enregistré"));
+    await tester.tap(find.textContaining("Toutes"));
     await tester.pumpAndSettle();
-    expect(find.textContaining('Jeu'), findsWidgets);
+    expect(find.textContaining('disponibles'), findsWidgets);
   });
 }
