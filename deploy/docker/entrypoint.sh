fb4b84cbe810664985cb47bb5b3dd94a092167b5
helderbetiol <helderbetiol@gmail.com>
2023-07-31 18:18:20 +0200

    Remove keyfile, add project name to rs.initiate

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,20 +1,9 @@
-chmod 400 /data/keyfile
-chown 999:999 /data/keyfile
-exec docker-entrypoint.sh mongod --bind_ip_all --replSet dbrs --keyFile /data/keyfile &
-sleep 5
+openssl rand -base64 768 > /data/keyfile
+chmod 400 /data/keyfile && chown 999:999 /data/keyfile
+exec docker-entrypoint.sh mongod --bind_ip_all --replSet rs0 --keyFile /data/keyfile &
+sleep 10
 mongosh -u admin -p adminpassword <<EOF
-var config = {
-    "_id": "dbrs",
-    "version": 1,
-    "members": [
-        {
-            "_id": 1,
-            "host": "dev_db:27017",
-            "priority": 3
-        },
-    ]
-};
-rs.initiate(config, { force: true });
+rs.initiate({"_id": "rs0", "members": [{"_id": 1,"host": "$1:27017"}]});
 rs.status();
 EOF
 sleep infinity
