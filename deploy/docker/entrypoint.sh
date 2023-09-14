2947282aac1dff8c7d7e872540fa42365c03e3fa
helderbetiol <helderbetiol@gmail.com>
2023-09-14 18:31:55 +0200

    chore(deploy) Replace sleep by mongo log check

--- deploy/docker/entrypoint.sh
+++ deploy/docker/entrypoint.sh
@@ -1,7 +1,20 @@
+#!/bin/bash
+LOGFILE=/var/log/mongodb/mongodb.log
 openssl rand -base64 768 > /data/keyfile
 chmod 400 /data/keyfile && chown 999:999 /data/keyfile
-exec docker-entrypoint.sh mongod --bind_ip_all --replSet rs0 --keyFile /data/keyfile &
-sleep 10
+exec docker-entrypoint.sh mongod --bind_ip_all --replSet rs0 --keyFile /data/keyfile --logpath $LOGFILE &
+
+# Wait until mongo logs that it's ready
+while [ ! -f $LOGFILE ]
+do
+  sleep 2 
+done
+while [ ! "$(grep 'Waiting for connections' $LOGFILE)" ] ; do
+    sleep 2
+    echo "Waiting for mongo to initialize..."
+done
+
+# Initiate replicaSet
 mongosh -u admin -p adminpassword <<EOF
 rs.initiate({"_id": "rs0", "members": [{"_id": 1,"host": "$1:27017"}]});
 rs.status();
