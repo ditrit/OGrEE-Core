ba59fa123d1a04be1e8999ac3e31b9364b5d65a1
helderbetiol <helderbetiol@gmail.com>
2023-09-22 11:40:18 +0200

    fix(deploy) add red message to webapp launch error

--- deploy/app/launch.ps1
+++ deploy/app/launch.ps1
@@ -9,6 +9,9 @@ $assetsDir = "${PWD}\assets\custom"
 $file = "${assetsDir}\.env"
 (Get-Content $file) -replace '8081', $portBack | Set-Content $file
 docker run --restart always --name ogree-superadmin -p ${portWeb}:80 -v ${assetsDir}:/usr/share/nginx/html/assets/assets/custom -d ogree-app:latest
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "UNABLE TO LAUNCH WEBAPP CONTAINER, CHECK ERROR ABOVE" -ForegroundColor red
+}
 
 cd ..\BACK\docker-backend
 docker run --rm -v ${PWD}:/workdir -w /workdir -e GOOS=windows golang go build -o ogree_app_backend.exe
