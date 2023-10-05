f40363f2d14187a5799ca16a83e331ece9d2f6b4
Franco Liberali <franco.liberali@gmail.com>
2023-10-05 17:02:58 +0200

    feat(cli): try to establish connection with ogree-3d on commands where connection is mandatory

--- CLI/controllers/commandController_test.go
+++ CLI/controllers/commandController_test.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"cli/models"
 	"cli/readline"
+	"fmt"
 	"net"
 	"sync"
 	"testing"
@@ -105,15 +106,67 @@ func TestConnect3DConnectsIfAlreadyConnectedAndDifferentUrlProvidedIsReachable(t
 	wg.Wait()
 }
 
+func TestInformOgree3DOptionalDoesNothingIfOgree3DNotConnected(t *testing.T) {
+	require.False(t, models.Ogree3D.IsConnected())
+	err := InformOgree3DOptional("Interact", -1, map[string]any{})
+	assert.Nil(t, err)
+}
+
+func TestInformOgree3DOptionalSendDataWhenOgree3DIsConnected(t *testing.T) {
+	fakeOgree3D(t, "3000")
+
+	err := Connect3D("localhost:3000")
+	require.Nil(t, err)
+	require.True(t, models.Ogree3D.IsConnected())
+
+	err = InformOgree3DOptional("Interact", -1, map[string]any{})
+	assert.Nil(t, err)
+}
+
+func TestInformOgree3DFailsIfOgree3DNotReachable(t *testing.T) {
+	require.False(t, models.Ogree3D.IsConnected())
+	State.Ogree3DURL = "localhost:3000"
+	err := InformOgree3D("Interact", -1, map[string]any{})
+	assert.ErrorContains(t, err, "OGrEE-3D is not reachable caused by OGrEE-3D (localhost:3000) unreachable\ndial tcp 127.0.0.1:3000: connect: connection refused")
+}
+
+func TestInformOgree3DEstablishConnectionIfOgree3DIsReachable(t *testing.T) {
+	require.False(t, models.Ogree3D.IsConnected())
+
+	State.Ogree3DURL = "localhost:3000"
+	fakeOgree3D(t, "3000")
+
+	err := InformOgree3D("Interact", -1, map[string]any{})
+	assert.Nil(t, err)
+}
+
+func TestInformOgree3DSendsDataIfEstablishConnectionWithOgree3DAlreadyEstablished(t *testing.T) {
+	fakeOgree3D(t, "3000")
+
+	err := Connect3D("localhost:3000")
+	require.Nil(t, err)
+	require.True(t, models.Ogree3D.IsConnected())
+
+	err = InformOgree3D("Interact", -1, map[string]any{})
+	assert.Nil(t, err)
+}
+
 func fakeOgree3D(t *testing.T, port string) *sync.WaitGroup {
 	ln, err := net.Listen("tcp", ":"+port)
 	require.Nil(t, err)
+
+	fmt.Println("Fake OGrEE-3D running on", ":"+port)
 
 	var wg sync.WaitGroup
 	wg.Add(1)
 
 	go func() {
-		conn, _ := ln.Accept()
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println("Fake OGrEE-3D could not accept connection")
+			return
+		}
+
 		buf := make([]byte, 1)
 		for {
 			_, err := conn.Read(buf)
@@ -121,8 +174,8 @@ func fakeOgree3D(t *testing.T, port string) *sync.WaitGroup {
 				break
 			}
 		}
+		fmt.Println("Fake OGrEE-3D connection broken")
 		conn.Close()
-		ln.Close()
 		wg.Done()
 	}()
 
