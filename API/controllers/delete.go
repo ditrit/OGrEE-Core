f32fdca9eb959058f304f0446f68a6cbfe65bca5
Franco Liberali <franco.liberali@gmail.com>
2023-10-26 17:02:54 +0200

    agregar al pr anterior add and remove tags from entities

--- API/controllers/delete.go
+++ API/controllers/delete.go
@@ -1,18 +1,13 @@
 package controllers
 
 import (
-	"log"
 	"p3/models"
 	u "p3/utils"
 )
 
 func deleteObject(entityStr string, id string, userRoles map[string]models.Role) *u.Error {
-	log.Println("-----------delete-------------")
-	log.Println(entityStr)
-	log.Println(id)
 	entity := u.EntityStrToInt(entityStr)
 	if entity == u.TAG {
-		log.Println("delete tag")
 		return models.DeleteTag(id)
 	} else if u.IsEntityNonHierarchical(entity) {
 		return models.DeleteNonHierarchicalObject(entityStr, id)
