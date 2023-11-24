3f4d17e5d3cb23883a4d53e75f65cbcbf424c98b
Franco Liberali <franco.liberali@gmail.com>
2023-11-24 10:03:49 +0100

    feat(cli): draw, undraw and update layer (#302)

--- CLI/controllers/commandController.go
+++ CLI/controllers/commandController.go
@@ -27,14 +27,16 @@ func PWD() string {
 	return State.CurrPath
 }
 
-func UnfoldPath(path string) ([]string, error) {
-	if strings.Contains(path, "*") {
-		_, subpaths, err := C.GetObjectsWildcard(path)
+func (controller Controller) UnfoldPath(path string) ([]string, error) {
+	if strings.Contains(path, "*") || models.PathHasLayer(path) {
+		_, subpaths, err := controller.GetObjectsWildcard(path)
 		return subpaths, err
 	}
+
 	if path == "_" {
 		return State.ClipBoard, nil
 	}
+
 	return []string{path}, nil
 }
 
@@ -1232,87 +1234,6 @@ func UnlinkObject(path string) error {
 	}
 	_, err = API.Request("PATCH", sourceUrl+"/unlink", nil, http.StatusOK)
 	return err
-}
-
-func objectCounter(parent map[string]interface{}) int {
-	count := 0
-	if parent != nil {
-		count += 1
-		if _, ok := parent["children"]; ok {
-			if arr, ok := parent["children"].([]interface{}); ok {
-				for _, childInf := range arr {
-					if child, ok := childInf.(map[string]interface{}); ok {
-						count += objectCounter(child)
-					}
-				}
-			}
-			if arr, ok := parent["children"].([]map[string]interface{}); ok {
-				for _, child := range arr {
-					count += objectCounter(child)
-				}
-			}
-		}
-	}
-	return count
-}
-
-// Unity UI will draw already existing objects
-// by retrieving the hierarchy. 'force' bool is useful
-// for scripting where the user can 'force' input if
-// the num objects to draw surpasses threshold
-func Draw(path string, depth int, force bool) error {
-	obj, err := C.GetObjectWithChildren(path, depth)
-	if err != nil {
-		return err
-	}
-
-	count := objectCounter(obj)
-	okToGo := true
-	if count > State.DrawThreshold && !force {
-		msg := "You are about to send " + strconv.Itoa(count) +
-			" objects to the Unity 3D client. " +
-			"Do you want to continue ? (y/n)\n"
-		(*State.Terminal).Write([]byte(msg))
-		(*State.Terminal).SetPrompt(">")
-		ans, _ := (*State.Terminal).Readline()
-		if ans != "y" && ans != "Y" {
-			okToGo = false
-		}
-	} else if force {
-		okToGo = true
-	} else if !force && count > State.DrawThreshold {
-		okToGo = false
-	}
-	if okToGo {
-		data := map[string]interface{}{"type": "create", "data": obj}
-		//0 to include the JSON filtration
-		unityErr := Ogree3D.Inform("Draw", 0, data)
-		if unityErr != nil {
-			return unityErr
-		}
-	}
-	return nil
-}
-
-func Undraw(x string) error {
-	var id string
-	if x == "" {
-		id = ""
-	} else {
-		obj, err := C.GetObject(x)
-		if err != nil {
-			return err
-		}
-		var ok bool
-		id, ok = obj["id"].(string)
-		if !ok {
-			return fmt.Errorf("this object has no id")
-		}
-	}
-
-	data := map[string]interface{}{"type": "delete", "data": id}
-
-	return Ogree3D.Inform("Undraw", 0, data)
 }
 
 func IsEntityDrawable(path string) (bool, error) {
