7e4ac396c9e671dfe717c33926c1f5456cc2692a
Fábio de Andrade Barboza <barbozafab@outlook.com>
2024-01-08 15:11:12 -0300

    feat(API) Better error messages on invalid ParentID

--- API/models/validateEntity.go
+++ API/models/validateEntity.go
@@ -89,17 +89,9 @@ func validateParent(ent string, entNum int, t map[string]interface{}) (map[strin
 		}
 
 		return nil, &u.Error{Type: u.ErrInvalidValue,
-			Message: "ParentID should correspond to Existing Rack or Device ID"}
+			Message: "ParentID should correspond to existing rack or device ID"}
 
 	case u.GROUP:
-		w, _ := GetObject(req, "device", u.RequestFilters{}, nil)
-		if w != nil {
-			parent["parent"] = "device"
-			parent["domain"] = w["domain"]
-			parent["id"] = w["id"]
-			return parent, nil
-		}
-
 		x, _ := GetObject(req, "rack", u.RequestFilters{}, nil)
 		if x != nil {
 			parent["parent"] = "rack"
@@ -125,7 +117,7 @@ func validateParent(ent string, entNum int, t map[string]interface{}) (map[strin
 		}
 
 		return nil, &u.Error{Type: u.ErrInvalidValue,
-			Message: "ParentID should correspond to Existing ID"}
+			Message: "ParentID should correspond to existing rack, room or building ID"}
 
 	case u.GENERIC:
 		x, _ := GetObject(req, "room", u.RequestFilters{}, nil)
@@ -137,7 +129,7 @@ func validateParent(ent string, entNum int, t map[string]interface{}) (map[strin
 		}
 
 		return nil, &u.Error{Type: u.ErrInvalidValue,
-			Message: "ParentID should correspond to Existing Room ID"}
+			Message: "ParentID should correspond to existing room ID"}
 	default:
 		parentInt := u.GetParentOfEntityByInt(entNum)
 		parentStr := u.EntityToString(parentInt)
@@ -151,8 +143,9 @@ func validateParent(ent string, entNum int, t map[string]interface{}) (map[strin
 		} else if err != nil {
 			println("ENTITY VALUE: ", ent)
 			println("We got Parent: ", parent, " with ID:", t["parentId"].(string))
+			fmt.Printf("parent should be %v\n", u.EntityToString(parentInt))
 			return nil, &u.Error{Type: u.ErrInvalidValue,
-				Message: "ParentID should correspond to Existing ID"}
+				Message: fmt.Sprintf("ParentID should correspond to existing %s ID", parentStr)}
 		}
 	}
 	return nil, nil
