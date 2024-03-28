bb2457c7c7d8b528f62b3506475b9212d91a238f
agustin <aguszorza@gmail.com>
2024-03-28 16:09:07 +0100

    test(cli) test create and update objects

--- CLI/controllers/create_test.go
+++ CLI/controllers/create_test.go
@@ -136,3 +136,452 @@ func TestCreateGenericWithTemplateWorks(t *testing.T) {
 	})
 	assert.Nil(t, err)
 }
+
+func TestCreateDomain(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	// domain with no parent
+	mockCreateObject(mockAPI, "domain", map[string]any{
+		"category":    "domain",
+		"id":          "dom1",
+		"name":        "dom1",
+		"parentId":    "",
+		"description": "",
+		"attributes":  map[string]any{},
+	})
+
+	err := controller.CreateObject("/Organisation/Domain/dom1", models.DOMAIN, map[string]any{
+		"category":    "domain",
+		"id":          "dom1",
+		"name":        "dom1",
+		"description": "",
+	})
+	assert.Nil(t, err)
+
+	// domain with parent
+	mockGetObjectByEntity(mockAPI, "domains", map[string]any{
+		"category": "domain",
+		"id":       "domParent",
+		"name":     "domParent",
+		"parentId": "",
+	})
+
+	mockCreateObject(mockAPI, "domain", map[string]any{
+		"category":    "domain",
+		"id":          "domParent.dom2",
+		"name":        "dom2",
+		"parentId":    "domParent",
+		"description": "",
+		"attributes":  map[string]any{},
+	})
+
+	err = controller.CreateObject("/Organisation/Domain/domParent/dom2", models.DOMAIN, map[string]any{
+		"category":    "domain",
+		"id":          "domParent.dom2",
+		"name":        "dom2",
+		"parentId":    "domParent",
+		"description": "",
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateBuildingInvalidSize(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "site",
+		"children": []any{},
+		"id":       "BASIC",
+		"name":     "BASIC",
+		"parentId": "",
+		"domain":   "test-domain",
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, map[string]any{
+		"category": "building",
+		"id":       "BASIC.A",
+		"name":     "A",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"heightUnit": "m",
+			"rotation":   30.5,
+			"posXY":      []float64{4.6666666666667, -2},
+			"posXYUnit":  "m",
+			"size":       []float64{},
+			"sizeUnit":   "m",
+		},
+	})
+	// returns nil but the object is not created
+	assert.Nil(t, err)
+}
+
+func TestCreateBuildingInvalidPosXY(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "site",
+		"children": []any{},
+		"id":       "BASIC",
+		"name":     "BASIC",
+		"parentId": "",
+		"domain":   "test-domain",
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, map[string]any{
+		"category": "building",
+		"id":       "BASIC.A",
+		"name":     "A",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"heightUnit": "m",
+			"rotation":   30.5,
+			"posXY":      []float64{},
+			"posXYUnit":  "m",
+			"size":       []float64{2, 2, 3},
+			"sizeUnit":   "m",
+		},
+	})
+	// returns nil but the object is not created
+	assert.Nil(t, err)
+}
+
+func TestCreateBuilding(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "site",
+		"children": []any{},
+		"id":       "BASIC",
+		"name":     "BASIC",
+		"parentId": "",
+		"domain":   "test-domain",
+	})
+
+	mockCreateObject(mockAPI, "building", map[string]any{
+		"category":    "building",
+		"id":          "BASIC.A",
+		"name":        "A",
+		"parentId":    "BASIC",
+		"domain":      "test-domain",
+		"description": "",
+		"attributes": map[string]any{
+			"height":     "5",
+			"heightUnit": "m",
+			"rotation":   `30.5`,
+			"posXY":      `[4.6666666666667, -2]`,
+			"posXYUnit":  "m",
+			"size":       `[3, 3]`,
+			"sizeUnit":   "m",
+		},
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, map[string]any{
+		"category": "building",
+		"id":       "BASIC.A",
+		"name":     "A",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"heightUnit": "m",
+			"rotation":   30.5,
+			"posXY":      []float64{4.6666666666667, -2},
+			"posXYUnit":  "m",
+			"size":       []float64{3, 3, 5},
+			"sizeUnit":   "m",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateRoomInvalidSize(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "building",
+		"children": []any{},
+		"id":       "BASIC.A",
+		"name":     "A",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, map[string]any{
+		"category": "room",
+		"id":       "BASIC.A.R1",
+		"name":     "R1",
+		"parentId": "BASIC.A",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"floorUnit":       "t",
+			"heightUnit":      "m",
+			"rotation":        30.5,
+			"axisOrientation": "+x+y",
+			"posXY":           []float64{4.6666666666667, -2},
+			"posXYUnit":       "m",
+			"size":            []float64{},
+			"sizeUnit":        "m",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateRoomInvalidPosXY(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "building",
+		"children": []any{},
+		"id":       "BASIC.A",
+		"name":     "A",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, map[string]any{
+		"category": "room",
+		"id":       "BASIC.A.R1",
+		"name":     "R1",
+		"parentId": "BASIC.A",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"floorUnit":       "t",
+			"heightUnit":      "m",
+			"rotation":        30.5,
+			"axisOrientation": "+x+y",
+			"posXY":           []float64{},
+			"posXYUnit":       "m",
+			"size":            []float64{2, 3, 3},
+			"sizeUnit":        "m",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateRoom(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "building",
+		"children": []any{},
+		"id":       "BASIC.A",
+		"name":     "A",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+	})
+
+	mockCreateObject(mockAPI, "room", map[string]any{
+		"category":    "room",
+		"id":          "BASIC.A.R1",
+		"name":        "R1",
+		"parentId":    "BASIC.A",
+		"domain":      "test-domain",
+		"description": "",
+		"attributes": map[string]any{
+			"floorUnit":       "t",
+			"height":          "5",
+			"heightUnit":      "m",
+			"axisOrientation": "+x+y",
+			"rotation":        `30.5`,
+			"posXY":           `[4.6666666666667, -2]`,
+			"posXYUnit":       "m",
+			"size":            `[3, 3]`,
+			"sizeUnit":        "m",
+		},
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, map[string]any{
+		"category": "room",
+		"id":       "BASIC.A.R1",
+		"name":     "R1",
+		"parentId": "BASIC.A",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"floorUnit":       "t",
+			"heightUnit":      "m",
+			"rotation":        30.5,
+			"axisOrientation": "+x+y",
+			"posXY":           []float64{4.6666666666667, -2},
+			"posXYUnit":       "m",
+			"size":            []float64{3, 3, 5},
+			"sizeUnit":        "m",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateRack(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "room",
+		"children": []any{},
+		"id":       "BASIC.A.R1",
+		"name":     "R1",
+		"parentId": "BASIC.A",
+		"domain":   "test-domain",
+	})
+
+	mockCreateObject(mockAPI, "rack", map[string]any{
+		"category":    "rack",
+		"id":          "BASIC.A.R1.A01",
+		"name":        "A01",
+		"parentId":    "BASIC.A.R1",
+		"domain":      "test-domain",
+		"description": "",
+		"attributes": map[string]any{
+			"height":     "47",
+			"heightUnit": "U",
+			"rotation":   `[45, 45, 45]`,
+			"posXYZ":     `[4.6666666666667, -2, 0]`,
+			"posXYUnit":  "m",
+			"size":       `[1, 1]`,
+			"sizeUnit":   "cm",
+		},
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1/A01", models.RACK, map[string]any{
+		"category": "rack",
+		"id":       "BASIC.A.R1.A01",
+		"name":     "A01",
+		"parentId": "BASIC.A.R1",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"heightUnit": "U",
+			"rotation":   []float64{45, 45, 45},
+			"posXYZ":     []float64{4.6666666666667, -2, 0},
+			"posXYUnit":  "m",
+			"size":       []float64{1, 1, 47},
+			"sizeUnit":   "cm",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateDevice(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "rack",
+		"children": []any{},
+		"id":       "BASIC.A.R1.A01",
+		"name":     "A01",
+		"parentId": "BASIC.A.R1",
+		"domain":   "test-domain",
+	})
+
+	mockCreateObject(mockAPI, "device", map[string]any{
+		"category":    "device",
+		"id":          "BASIC.A.R1.A01.D1",
+		"name":        "D1",
+		"parentId":    "BASIC.A.R1.A01",
+		"domain":      "test-domain",
+		"description": "",
+		"attributes": map[string]any{
+			"height":      "47",
+			"heightUnit":  "U",
+			"orientation": "front",
+			"size":        `[1,1]`,
+			"sizeUnit":    "cm",
+		},
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1/A01/D1", models.DEVICE, map[string]any{
+		"category": "device",
+		"id":       "BASIC.A.R1.A01.D1",
+		"name":     "D1",
+		"parentId": "BASIC.A.R1.A01",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"height":      "47",
+			"heightUnit":  "U",
+			"orientation": "front",
+			"size":        []float64{1, 1},
+			"sizeUnit":    "cm",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateDeviceWithSizeU(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "rack",
+		"children": []any{},
+		"id":       "BASIC.A.R1.A01",
+		"name":     "A01",
+		"parentId": "BASIC.A.R1",
+		"domain":   "test-domain",
+	})
+
+	mockCreateObject(mockAPI, "device", map[string]any{
+		"category":    "device",
+		"id":          "BASIC.A.R1.A01.D1",
+		"name":        "D1",
+		"parentId":    "BASIC.A.R1.A01",
+		"domain":      "test-domain",
+		"description": "",
+		"attributes": map[string]any{
+			"height":      "89",
+			"sizeU":       "2",
+			"heightUnit":  "U",
+			"orientation": "front",
+			"size":        `[1,1]`,
+			"sizeUnit":    "cm",
+		},
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1/A01/D1", models.DEVICE, map[string]any{
+		"category": "device",
+		"id":       "BASIC.A.R1.A01.D1",
+		"name":     "D1",
+		"parentId": "BASIC.A.R1.A01",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"sizeU":       2,
+			"heightUnit":  "U",
+			"orientation": "front",
+			"size":        []float64{1, 1},
+			"sizeUnit":    "cm",
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func TestCreateGroup(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+
+	mockGetObject(mockAPI, map[string]any{
+		"category": "site",
+		"children": []any{},
+		"id":       "BASIC",
+		"name":     "BASIC",
+		"parentId": "BASIC",
+		"domain":   "test-domain",
+	})
+
+	mockCreateObject(mockAPI, "group", map[string]any{
+		"attributes": map[string]any{
+			"content": "R1,R2",
+		},
+		"category":    "group",
+		"description": "",
+		"domain":      "test-domain",
+		"name":        "G1",
+		"parentId":    "BASIC",
+	})
+
+	err := controller.CreateObject("/Physical/BASIC/G1", models.GROUP, map[string]any{
+		"attributes": map[string]any{
+			"content": []string{"R1", "R2"},
+		},
+		"category":    "group",
+		"description": "",
+		"domain":      "test-domain",
+		"name":        "G1",
+		"parentId":    "BASIC",
+	})
+	assert.Nil(t, err)
+}
--- CLI/controllers/update_test.go
+++ CLI/controllers/update_test.go
@@ -3,6 +3,7 @@ package controllers_test
 import (
 	"cli/controllers"
 	"cli/models"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -66,4 +67,185 @@ func TestUpdateTagSlug(t *testing.T) {
 
 	_, err := controller.UpdateObj(path, dataUpdate, false)
 	assert.Nil(t, err)
+}
+
+func TestUpdateRoomTilesColor(t *testing.T) {
+	controller, mockAPI, mockOgree3D, _ := newControllerWithMocks(t)
+
+	room := copyMap(roomWithoutChildren)
+	room["attributes"] = map[string]any{
+		"tilesColor": "aaaaaa",
+	}
+	newColor := "aaaaab"
+	updatedRoom := copyMap(room)
+	updatedRoom["attributes"].(map[string]any)["tilesColor"] = newColor
+	dataUpdate := updatedRoom["attributes"].(map[string]any)
+	entity := models.ROOM
+
+	path := "/Physical/" + strings.Replace(room["id"].(string), ".", "/", -1)
+	message := map[string]any{
+		"type": "interact",
+		"data": map[string]any{
+			"id":    room["id"],
+			"param": "tilesColor",
+			"value": newColor,
+		},
+	}
+
+	mockOgree3D.On("InformOptional", "UpdateObj", entity, message).Return(nil)
+
+	mockGetObject(mockAPI, room)
+
+	dataUpdated := copyMap(room)
+	dataUpdated["attributes"].(map[string]any)["tilesColor"] = newColor
+
+	mockUpdateObject(mockAPI, dataUpdate, dataUpdated)
+
+	controllers.State.ObjsForUnity = controllers.SetObjsForUnity([]string{"all"})
+
+	result, err := controller.UpdateObj(path, dataUpdate, false)
+	assert.Nil(t, err)
+	assert.Equal(t, result["data"].(map[string]any)["attributes"].(map[string]any)["tilesColor"], newColor)
+	mockOgree3D.AssertCalled(t, "InformOptional", "UpdateObj", entity, message)
+}
+
+func TestUpdateRoomTilesName(t *testing.T) {
+	controller, mockAPI, mockOgree3D, _ := newControllerWithMocks(t)
+
+	room := copyMap(roomWithoutChildren)
+	room["attributes"] = map[string]any{
+		"tilesName": "t1",
+	}
+	newName := "t2"
+	updatedRoom := copyMap(room)
+	updatedRoom["attributes"].(map[string]any)["tilesName"] = newName
+	dataUpdate := updatedRoom["attributes"].(map[string]any)
+	entity := models.ROOM
+
+	path := "/Physical/" + strings.Replace(room["id"].(string), ".", "/", -1)
+	message := map[string]any{
+		"type": "interact",
+		"data": map[string]any{
+			"id":    room["id"],
+			"param": "tilesName",
+			"value": newName,
+		},
+	}
+
+	mockOgree3D.On("InformOptional", "UpdateObj", entity, message).Return(nil)
+
+	mockGetObject(mockAPI, room)
+
+	dataUpdated := copyMap(room)
+	dataUpdated["attributes"].(map[string]any)["tilesName"] = newName
+
+	mockUpdateObject(mockAPI, dataUpdate, dataUpdated)
+
+	controllers.State.ObjsForUnity = controllers.SetObjsForUnity([]string{"all"})
+
+	result, err := controller.UpdateObj(path, dataUpdate, false)
+	assert.Nil(t, err)
+	assert.Equal(t, result["data"].(map[string]any)["attributes"].(map[string]any)["tilesName"], newName)
+	mockOgree3D.AssertCalled(t, "InformOptional", "UpdateObj", entity, message)
+}
+
+func TestUpdateRackU(t *testing.T) {
+	controller, mockAPI, mockOgree3D, _ := newControllerWithMocks(t)
+	rack := copyMap(rack2)
+	rack["attributes"] = map[string]any{
+		"U": true,
+	}
+	updatedRack := copyMap(rack)
+	updatedRack["attributes"].(map[string]any)["U"] = false
+	dataUpdate := updatedRack["attributes"].(map[string]any)
+	entity := models.RACK
+
+	path := "/Physical/" + strings.Replace(rack["id"].(string), ".", "/", -1)
+	message := map[string]any{
+		"type": "interact",
+		"data": map[string]any{
+			"id":    rack["id"],
+			"param": "U",
+			"value": false,
+		},
+	}
+
+	mockOgree3D.On("InformOptional", "UpdateObj", entity, message).Return(nil)
+
+	mockGetObject(mockAPI, rack)
+	mockUpdateObject(mockAPI, dataUpdate, updatedRack)
+
+	controllers.State.ObjsForUnity = controllers.SetObjsForUnity([]string{"all"})
+
+	result, err := controller.UpdateObj(path, dataUpdate, false)
+	assert.Nil(t, err)
+	assert.False(t, result["data"].(map[string]any)["attributes"].(map[string]any)["U"].(bool))
+	mockOgree3D.AssertCalled(t, "InformOptional", "UpdateObj", entity, message)
+}
+
+func TestUpdateDeviceAlpha(t *testing.T) {
+	controller, mockAPI, mockOgree3D, _ := newControllerWithMocks(t)
+	device := copyMap(chassis)
+	device["attributes"].(map[string]any)["alpha"] = true
+	updatedDevice := copyMap(device)
+	updatedDevice["attributes"].(map[string]any)["alpha"] = false
+	dataUpdate := updatedDevice["attributes"].(map[string]any)
+	entity := models.DEVICE
+
+	path := "/Physical/" + strings.Replace(device["id"].(string), ".", "/", -1)
+	message := map[string]any{
+		"type": "interact",
+		"data": map[string]any{
+			"id":    device["id"],
+			"param": "alpha",
+			"value": false,
+		},
+	}
+
+	mockOgree3D.On("InformOptional", "UpdateObj", entity, message).Return(nil)
+
+	mockGetObject(mockAPI, device)
+	mockUpdateObject(mockAPI, dataUpdate, updatedDevice)
+
+	controllers.State.ObjsForUnity = controllers.SetObjsForUnity([]string{"all"})
+
+	result, err := controller.UpdateObj(path, dataUpdate, false)
+	assert.Nil(t, err)
+	assert.False(t, result["data"].(map[string]any)["attributes"].(map[string]any)["alpha"].(bool))
+	mockOgree3D.AssertCalled(t, "InformOptional", "UpdateObj", entity, message)
+}
+
+func TestUpdateGroupContent(t *testing.T) {
+	controller, mockAPI, mockOgree3D, _ := newControllerWithMocks(t)
+	group := copyMap(rackGroup)
+	group["attributes"] = map[string]any{
+		"content": "A,B",
+	}
+	newValue := "A,B,C"
+	updatedGroup := copyMap(group)
+	updatedGroup["attributes"].(map[string]any)["content"] = newValue
+	dataUpdate := updatedGroup["attributes"].(map[string]any)
+	entity := models.GROUP
+
+	path := "/Physical/" + strings.Replace(group["id"].(string), ".", "/", -1)
+	message := map[string]any{
+		"type": "interact",
+		"data": map[string]any{
+			"id":    group["id"],
+			"param": "content",
+			"value": newValue,
+		},
+	}
+
+	mockOgree3D.On("InformOptional", "UpdateObj", entity, message).Return(nil)
+
+	mockGetObject(mockAPI, group)
+	mockUpdateObject(mockAPI, dataUpdate, updatedGroup)
+
+	controllers.State.ObjsForUnity = controllers.SetObjsForUnity([]string{"all"})
+
+	result, err := controller.UpdateObj(path, dataUpdate, false)
+	assert.Nil(t, err)
+	assert.Equal(t, result["data"].(map[string]any)["attributes"].(map[string]any)["content"].(string), newValue)
+	mockOgree3D.AssertCalled(t, "InformOptional", "UpdateObj", entity, message)
 }
