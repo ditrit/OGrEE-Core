5bf7cd9467ae5a45f08876dc1b1b834f869e575e
agustin <aguszorza@gmail.com>
2024-04-09 15:57:39 +0200

    test(cli) add error tests

--- CLI/controllers/create_test.go
+++ CLI/controllers/create_test.go
@@ -1,11 +1,37 @@
 package controllers_test
 
 import (
+	"cli/controllers"
 	"cli/models"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
+
+var baseSite = map[string]any{
+	"category": "site",
+	"children": []any{},
+	"id":       "BASIC",
+	"name":     "BASIC",
+	"parentId": "",
+	"domain":   "test-domain",
+}
+
+var baseBuilding = map[string]any{
+	"category": "building",
+	"id":       "BASIC.A",
+	"name":     "A",
+	"parentId": "BASIC",
+	"domain":   "test-domain",
+	"attributes": map[string]any{
+		"heightUnit": "m",
+		"rotation":   30.5,
+		"posXY":      []float64{4.6666666666667, -2},
+		"posXYUnit":  "m",
+		"size":       []float64{3, 3, 5},
+		"sizeUnit":   "m",
+	},
+}
 
 var createRoom = map[string]any{
 	"category": "room",
@@ -13,6 +39,14 @@ var createRoom = map[string]any{
 	"name":     "R1",
 	"parentId": "BASIC.A",
 	"domain":   "test-domain",
+}
+
+func TestCreateObjectInvalidPath(t *testing.T) {
+	controller, _, _ := layersSetup(t)
+
+	err := controller.CreateObject("/.", models.RACK, map[string]any{})
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, "Invalid path name provided for OCLI object creation")
 }
 
 func TestCreateObjectErrorParentNotFound(t *testing.T) {
@@ -195,78 +229,57 @@ func TestCreateDomain(t *testing.T) {
 
 func TestCreateBuildingInvalidSize(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
+	buildingInvalidSize := copyMap(baseBuilding)
+	buildingInvalidSize["attributes"].(map[string]any)["size"] = []float64{}
 
-	mockGetObject(mockAPI, map[string]any{
-		"category": "site",
-		"children": []any{},
-		"id":       "BASIC",
-		"name":     "BASIC",
-		"parentId": "",
-		"domain":   "test-domain",
-	})
+	mockGetObject(mockAPI, baseSite)
 
-	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, map[string]any{
-		"category": "building",
-		"id":       "BASIC.A",
-		"name":     "A",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-		"attributes": map[string]any{
-			"heightUnit": "m",
-			"rotation":   30.5,
-			"posXY":      []float64{4.6666666666667, -2},
-			"posXYUnit":  "m",
-			"size":       []float64{},
-			"sizeUnit":   "m",
-		},
-	})
+	// with state.DebugLvl = 0
+	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, buildingInvalidSize)
 	// returns nil but the object is not created
 	assert.Nil(t, err)
+
+	// with state.DebugLvl > 0
+	controllers.State.DebugLvl = 1
+	mockGetObject(mockAPI, baseSite)
+	err = controller.CreateObject("/Physical/BASIC/A", models.BLDG, buildingInvalidSize)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, "Invalid size attribute provided."+
+		" \nIt must be an array/list/vector with 3 elements."+
+		" Please refer to the wiki or manual reference"+
+		" for more details on how to create objects "+
+		"using this syntax")
+	controllers.State.DebugLvl = 0
 }
 
 func TestCreateBuildingInvalidPosXY(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
+	buildingInvalidPosXY := copyMap(baseBuilding)
+	buildingInvalidPosXY["attributes"].(map[string]any)["posXY"] = []float64{}
 
-	mockGetObject(mockAPI, map[string]any{
-		"category": "site",
-		"children": []any{},
-		"id":       "BASIC",
-		"name":     "BASIC",
-		"parentId": "",
-		"domain":   "test-domain",
-	})
-
-	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, map[string]any{
-		"category": "building",
-		"id":       "BASIC.A",
-		"name":     "A",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-		"attributes": map[string]any{
-			"heightUnit": "m",
-			"rotation":   30.5,
-			"posXY":      []float64{},
-			"posXYUnit":  "m",
-			"size":       []float64{2, 2, 3},
-			"sizeUnit":   "m",
-		},
-	})
+	// with state.DebugLvl = 0
+	mockGetObject(mockAPI, baseSite)
+	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, copyMap(buildingInvalidPosXY))
 	// returns nil but the object is not created
 	assert.Nil(t, err)
+
+	// with state.DebugLvl > 0
+	controllers.State.DebugLvl = 1
+	mockGetObject(mockAPI, baseSite)
+	err = controller.CreateObject("/Physical/BASIC/A", models.BLDG, buildingInvalidPosXY)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, "Invalid posXY attribute provided."+
+		" \nIt must be an array/list/vector with 2 elements."+
+		" Please refer to the wiki or manual reference"+
+		" for more details on how to create objects "+
+		"using this syntax")
+	controllers.State.DebugLvl = 0
 }
 
 func TestCreateBuilding(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
 
-	mockGetObject(mockAPI, map[string]any{
-		"category": "site",
-		"children": []any{},
-		"id":       "BASIC",
-		"name":     "BASIC",
-		"parentId": "",
-		"domain":   "test-domain",
-	})
-
+	mockGetObject(mockAPI, baseSite)
 	mockCreateObject(mockAPI, "building", map[string]any{
 		"category":    "building",
 		"id":          "BASIC.A",
@@ -285,37 +298,14 @@ func TestCreateBuilding(t *testing.T) {
 		},
 	})
 
-	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, map[string]any{
-		"category": "building",
-		"id":       "BASIC.A",
-		"name":     "A",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-		"attributes": map[string]any{
-			"heightUnit": "m",
-			"rotation":   30.5,
-			"posXY":      []float64{4.6666666666667, -2},
-			"posXYUnit":  "m",
-			"size":       []float64{3, 3, 5},
-			"sizeUnit":   "m",
-		},
-	})
+	err := controller.CreateObject("/Physical/BASIC/A", models.BLDG, copyMap(baseBuilding))
 	assert.Nil(t, err)
 }
 
 func TestCreateRoomInvalidSize(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
-
-	mockGetObject(mockAPI, map[string]any{
-		"category": "building",
-		"children": []any{},
-		"id":       "BASIC.A",
-		"name":     "A",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-	})
-
-	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, map[string]any{
+	roomsBuilding := copyMap(baseBuilding)
+	room := map[string]any{
 		"category": "room",
 		"id":       "BASIC.A.R1",
 		"name":     "R1",
@@ -331,23 +321,30 @@ func TestCreateRoomInvalidSize(t *testing.T) {
 			"size":            []float64{},
 			"sizeUnit":        "m",
 		},
-	})
+	}
+
+	// with state.DebugLvl = 0
+	mockGetObject(mockAPI, roomsBuilding)
+	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, room)
 	assert.Nil(t, err)
+
+	// with state.DebugLvl > 0
+	controllers.State.DebugLvl = 1
+	mockGetObject(mockAPI, roomsBuilding)
+	err = controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, room)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, "Invalid size attribute provided."+
+		" \nIt must be an array/list/vector with 3 elements."+
+		" Please refer to the wiki or manual reference"+
+		" for more details on how to create objects "+
+		"using this syntax")
+	controllers.State.DebugLvl = 0
 }
 
 func TestCreateRoomInvalidPosXY(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
-
-	mockGetObject(mockAPI, map[string]any{
-		"category": "building",
-		"children": []any{},
-		"id":       "BASIC.A",
-		"name":     "A",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-	})
-
-	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, map[string]any{
+	roomsBuilding := copyMap(baseBuilding)
+	room := map[string]any{
 		"category": "room",
 		"id":       "BASIC.A.R1",
 		"name":     "R1",
@@ -363,21 +360,31 @@ func TestCreateRoomInvalidPosXY(t *testing.T) {
 			"size":            []float64{2, 3, 3},
 			"sizeUnit":        "m",
 		},
-	})
+	}
+
+	// with state.DebugLvl = 0
+	mockGetObject(mockAPI, roomsBuilding)
+
+	err := controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, copyMap(room))
 	assert.Nil(t, err)
+
+	// with state.DebugLvl > 0
+	controllers.State.DebugLvl = 1
+	mockGetObject(mockAPI, roomsBuilding)
+	err = controller.CreateObject("/Physical/BASIC/A/R1", models.ROOM, room)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, "Invalid posXY attribute provided."+
+		" \nIt must be an array/list/vector with 2 elements."+
+		" Please refer to the wiki or manual reference"+
+		" for more details on how to create objects "+
+		"using this syntax")
+	controllers.State.DebugLvl = 0
 }
 
 func TestCreateRoom(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
 
-	mockGetObject(mockAPI, map[string]any{
-		"category": "building",
-		"children": []any{},
-		"id":       "BASIC.A",
-		"name":     "A",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-	})
+	mockGetObject(mockAPI, copyMap(baseBuilding))
 
 	mockCreateObject(mockAPI, "room", map[string]any{
 		"category":    "room",
@@ -417,6 +424,54 @@ func TestCreateRoom(t *testing.T) {
 		},
 	})
 	assert.Nil(t, err)
+}
+
+func TestCreateRackInvalidSize(t *testing.T) {
+	controller, mockAPI, _ := layersSetup(t)
+	room := map[string]any{
+		"category": "room",
+		"children": []any{},
+		"id":       "BASIC.A.R1",
+		"name":     "R1",
+		"parentId": "BASIC.A",
+		"domain":   "test-domain",
+	}
+	rack := map[string]any{
+		"category": "rack",
+		"id":       "BASIC.A.R1.A01",
+		"name":     "A01",
+		"parentId": "BASIC.A.R1",
+		"domain":   "test-domain",
+		"attributes": map[string]any{
+			"heightUnit": "U",
+			"rotation":   []float64{45, 45, 45},
+			"posXYZ":     []float64{4.6666666666667, -2, 0},
+			"posXYUnit":  "m",
+			"size":       []float64{},
+			"sizeUnit":   "cm",
+		},
+	}
+
+	// with state.DebugLvl = 0
+	mockGetObject(mockAPI, room)
+	err := controller.CreateObject("/Physical/BASIC/A/R1/A01", models.RACK, rack)
+	assert.Nil(t, err)
+
+	// with state.DebugLvl > 0
+	controllers.State.DebugLvl = 1
+	mockGetObject(mockAPI, room)
+	err = controller.CreateObject("/Physical/BASIC/A/R1/A01", models.RACK, rack)
+	assert.NotNil(t, err)
+	assert.ErrorContains(t, err, "Invalid size attribute/template provided."+
+		" \nThe size must be an array/list/vector with "+
+		"3 elements."+"\n\nIf you have provided a"+
+		" template, please check that you are referring to "+
+		"an existing template"+
+		"\n\nFor more information "+
+		"please refer to the wiki or manual reference"+
+		" for more details on how to create objects "+
+		"using this syntax")
+	controllers.State.DebugLvl = 0
 }
 
 func TestCreateRack(t *testing.T) {
@@ -583,14 +638,7 @@ func TestCreateDeviceWithSizeU(t *testing.T) {
 func TestCreateGroup(t *testing.T) {
 	controller, mockAPI, _ := layersSetup(t)
 
-	mockGetObject(mockAPI, map[string]any{
-		"category": "site",
-		"children": []any{},
-		"id":       "BASIC",
-		"name":     "BASIC",
-		"parentId": "BASIC",
-		"domain":   "test-domain",
-	})
+	mockGetObject(mockAPI, baseSite)
 
 	mockCreateObject(mockAPI, "group", map[string]any{
 		"attributes": map[string]any{
