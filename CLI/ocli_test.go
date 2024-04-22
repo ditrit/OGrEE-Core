c76958c7ec5e4dc9132a728b95ed1241b7f565ef
agustin <aguszorza@gmail.com>
2024-04-22 16:30:16 +0200

    test(cli) add astutil tests

--- CLI/ocli_test.go
+++ CLI/ocli_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"testing"
 
@@ -37,3 +38,69 @@ func TestAddLineError(t *testing.T) {
 	assert.Len(t, fileParser.lineErrors, 2)
 	assert.Equal(t, fmt.Sprintf("  LINE#: %d\tCOMMAND:%s", lineNumber, line), fileParser.lineErrors[1])
 }
+
+func TestParseFileError(t *testing.T) {
+	invalidPath := "/invalid/path/file.ocli"
+	_, err := parseFile(invalidPath)
+	assert.ErrorContains(t, err, "open "+invalidPath+": no such file or directory")
+}
+
+func TestParseFile(t *testing.T) {
+	basePath := t.TempDir() // temporary directory that will be deleted after the tests have finished
+	fileContent := ".var:siteName=siteB\n"
+	fileContent += "+site:$siteName\n"
+	fileContent += "+bd:/P/$siteName/blgdB@[0,0]@-90@[25,29.4,1]\n\n"
+	fileContent += "//This is a comment line\n"
+	fileContent += "+ro:/P/$siteName/blgdB/R2@[0,0]@0@[22.8,19.8,0.5]@+x+y\n\n"
+	fileContent += "for i in 1..2 {                                                            \\\n"
+	fileContent += "	.var:multbyten=eval 10*$i;                                             \\\n"
+	fileContent += "	+rk:/P/$siteName/blgdB/R2/A${multbyten}@[$i,2]@t@[0,0,180]@[60,120,42] \\\n"
+	fileContent += "}\n"
+
+	filename := "parse_test_file.ocli"
+	filePath := basePath + "/" + filename
+
+	err := os.WriteFile(filePath, []byte(fileContent), 0644)
+
+	if err != nil {
+		t.Errorf("an error ocurred while creating the test file: %s", err)
+	}
+	parsedLines, err := parseFile(filePath)
+	if err != nil {
+		t.Errorf("an error ocurred parsing the file: %s", err)
+	}
+	assert.Len(t, parsedLines, 5)
+	assert.Equal(t, "siteB", parsedLines[0].root.(*assignNode).val.(*valueNode).val)
+	assert.Equal(t, "siteName", parsedLines[0].root.(*assignNode).variable)
+	assert.IsType(t, &createSiteNode{}, parsedLines[1].root)
+	assert.IsType(t, &createBuildingNode{}, parsedLines[2].root)
+	assert.IsType(t, &createRoomNode{}, parsedLines[3].root)
+	assert.IsType(t, &forRangeNode{}, parsedLines[4].root)
+}
+
+func TestNewStackTraceError(t *testing.T) {
+	err := fmt.Errorf("my-error")
+	stackTrace := newStackTraceError(err, "my_file", "line", 1)
+	msg := "Stack trace (most recent call last):\n"
+	msg += stackTrace.history + "Error : " + err.Error()
+	assert.Equal(t, msg, stackTrace.Error())
+}
+
+// func TestLoadFile(t *testing.T) {
+// 	basePath := t.TempDir() // temporary directory that will be deleted after the tests have finished
+// 	fileContent := ".var:siteName=siteB\n"
+
+// 	filename := "load_test_file.ocli"
+// 	filePath := basePath + "/" + filename
+
+// 	fmt.Println(cmd.State)
+// 	err := os.WriteFile(filePath, []byte(fileContent), 0644)
+
+// 	if err != nil {
+// 		t.Errorf("an error ocurred while creating the test file: %s", err)
+// 	}
+// 	err = LoadFile(filePath)
+// 	if err != nil {
+// 		t.Errorf("an error ocurred parsing the file: %s", err)
+// 	}
+// }
