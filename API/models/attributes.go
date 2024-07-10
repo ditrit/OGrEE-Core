a2d500268327f56e85a142eabe8757e74f0ceaca
cedrok <cvautrai@student.42.fr>
2024-07-10 18:33:26 +0200

    fix(api) check coherence between sizeU and height

--- API/models/attributes.go
+++ API/models/attributes.go
@@ -10,6 +10,8 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+const RACKUNIT = 0.04445 //meter
+
 func validateAttributes(entity int, data, parent map[string]any) *u.Error {
 	attributes := data["attributes"].(map[string]any)
 	switch entity {
@@ -21,8 +23,13 @@ func validateAttributes(entity int, data, parent map[string]any) *u.Error {
 			return err
 		}
 	case u.DEVICE:
-		var deviceSlots []string
 		var err *u.Error
+		if attributes["sizeU"] != nil && attributes["height"] != nil {
+			if err = checkSizeUAndHeight(attributes); err != nil {
+				return err
+			}
+		}
+		var deviceSlots []string
 		if deviceSlots, err = slotToValidSlice(attributes); err != nil {
 			return err
 		}
@@ -67,8 +74,6 @@ func validateDeviceSlots(deviceSlots []string, deviceName, deviceParentd string)
 						Message: "Invalid slot: one or more requested slots are already in use"}
 				}
 			}
-		} else {
-			// fmt.Println(err)
 		}
 	}
 	return nil
@@ -150,5 +155,32 @@ func setCorridorColor(attributes map[string]any) {
 		attributes["color"] = "990000"
 	} else if attributes["temperature"] == "cold" {
 		attributes["color"] = "000099"
+	}
+}
+
+// Check if sizeU and height are coherents
+func checkSizeUAndHeight(attributes map[string]any) *u.Error {
+	sizeU, err := u.GetFloat(attributes["sizeU"])
+	if err != nil {
+		return &u.Error{
+			Type:    u.ErrBadFormat,
+			Message: err.Error(),
+		}
+	}
+	height := attributes["height"]
+	h := sizeU * RACKUNIT
+	switch heightUnit := attributes["heightUnit"]; heightUnit {
+	case "cm":
+		h *= 100
+	case "mm":
+		h *= 1000
+	}
+	if height == h {
+		return nil
+	} else {
+		return &u.Error{
+			Type:    u.ErrBadFormat,
+			Message: "sizeU and height are not consistent",
+		}
 	}
 }
