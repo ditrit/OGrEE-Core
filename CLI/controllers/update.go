41da081b39ead2b5d52cb76924be57f1c1a82925
cedrok <cvautrai@student.42.fr>
2024-07-10 14:54:46 +0200

    fix(cli) move sizeU/height related code to dedicated file + related tests

--- CLI/controllers/update.go
+++ CLI/controllers/update.go
@@ -2,7 +2,6 @@ package controllers
 
 import (
 	"cli/models"
-	"cli/utils"
 	"net/http"
 )
 
@@ -17,40 +16,8 @@ func (controller Controller) UpdateObj(pathStr string, data map[string]any, with
 		category = obj["category"].(string)
 	}
 
-	if category == models.EntityToString(models.DEVICE) && data["attributes"] != nil {
-		newAttrs, err := models.MapStringAny(data["attributes"])
-		if err != nil {
-			return nil, err
-		}
-		currentAttrs, _ := models.MapStringAny(obj["attributes"])
-		if newAttrs["sizeU"] != nil {
-			sizeU, err := utils.GetFloat(newAttrs["sizeU"])
-			if err != nil {
-				return nil, err
-			}
-			var height = sizeU * models.RACKUNIT
-			switch heightUnit := currentAttrs["heightUnit"]; heightUnit {
-			case "cm":
-				height *= 100
-			case "mm":
-				height *= 1000
-			}
-			newAttrs["height"] = height
-		}
-		if newAttrs["height"] != nil {
-			height, err := utils.GetFloat(newAttrs["height"])
-			if err != nil {
-				return nil, err
-			}
-			var sizeU = height / models.RACKUNIT
-			switch heightUnit := currentAttrs["heightUnit"]; heightUnit {
-			case "cm":
-				sizeU /= 100
-			case "mm":
-				sizeU /= 1000
-			}
-			newAttrs["sizeU"] = sizeU
-		}
+	if category == models.EntityToString(models.DEVICE) {
+		models.ComputeSizeUAndHeight(obj, data)
 	}
 
 	url, err := controller.ObjectUrl(pathStr, 0)
